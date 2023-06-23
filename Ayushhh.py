43f55245683bdd0872e950631bf5f70465ee19cf
Ayush <ayushjav.ochirbal@yahoo.com>
2023-06-23 15:51:19 +0800

    hh

--- Ayushhh.py
+++ Ayushhh.py
@@ -1,7 +1,9 @@
-G' 
-fjfjsv
-5= fjfjsv
-5+6
-11
 
+my_list_str = ["ayush" , "sugaraa" , "muugii" , "nomin" , "orgil"]
+print (my_list_str[3])
+print 
+
+
+my_list_num = [10, 30, 50.5]
+type(my_list_num)
 
